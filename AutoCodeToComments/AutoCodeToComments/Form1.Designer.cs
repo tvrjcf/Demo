9fd0b3384dfb0476c364ad7b6c5a1082da611628
jian wei <190565307@qq.com>
2018-07-26 09:50:37 +0800

    AutoCodeToComments

---
+++ AutoCodeToComments/AutoCodeToComments/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AutoCodeToComments
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtb_View = new System.Windows.Forms.RichTextBox();
+            this.dgv_List = new System.Windows.Forms.DataGridView();
+            this.btn_SelectFile = new System.Windows.Forms.Button();
+            this.tb_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_LoadData = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TABLE_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COLUMN_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COMMENTS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_CreateCode = new System.Windows.Forms.Button();
+            this.cbb_Type = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_List)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(829, 455);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(821, 429);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.rtb_View);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(751, 429);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbb_Type);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tb_FilePath);
+            this.groupBox1.Controls.Add(this.btn_CreateCode);
+            this.groupBox1.Controls.Add(this.btn_LoadData);
+            this.groupBox1.Controls.Add(this.btn_SelectFile);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(815, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgv_List);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(815, 366);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "数据";
+            // 
+            // rtb_View
+            // 
+            this.rtb_View.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_View.Location = new System.Drawing.Point(3, 3);
+            this.rtb_View.Name = "rtb_View";
+            this.rtb_View.Size = new System.Drawing.Size(745, 423);
+            this.rtb_View.TabIndex = 0;
+            this.rtb_View.Text = "";
+            // 
+            // dgv_List
+            // 
+            this.dgv_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_List.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.TABLE_NAME,
+            this.COLUMN_NAME,
+            this.COMMENTS});
+            this.dgv_List.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_List.Location = new System.Drawing.Point(3, 17);
+            this.dgv_List.Name = "dgv_List";
+            this.dgv_List.RowTemplate.Height = 23;
+            this.dgv_List.Size = new System.Drawing.Size(809, 346);
+            this.dgv_List.TabIndex = 0;
+            // 
+            // btn_SelectFile
+            // 
+            this.btn_SelectFile.Location = new System.Drawing.Point(416, 18);
+            this.btn_SelectFile.Name = "btn_SelectFile";
+            this.btn_SelectFile.Size = new System.Drawing.Size(39, 23);
+            this.btn_SelectFile.TabIndex = 0;
+            this.btn_SelectFile.Text = "...";
+            this.btn_SelectFile.UseVisualStyleBackColor = true;
+            this.btn_SelectFile.Click += new System.EventHandler(this.btn_SelectFile_Click);
+            // 
+            // tb_FilePath
+            // 
+            this.tb_FilePath.Location = new System.Drawing.Point(42, 19);
+            this.tb_FilePath.Name = "tb_FilePath";
+            this.tb_FilePath.Size = new System.Drawing.Size(361, 21);
+            this.tb_FilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件";
+            // 
+            // btn_LoadData
+            // 
+            this.btn_LoadData.Location = new System.Drawing.Point(461, 18);
+            this.btn_LoadData.Name = "btn_LoadData";
+            this.btn_LoadData.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadData.TabIndex = 0;
+            this.btn_LoadData.Text = "加载数据";
+            this.btn_LoadData.UseVisualStyleBackColor = true;
+            this.btn_LoadData.Click += new System.EventHandler(this.btn_LoadData_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 50;
+            // 
+            // TABLE_NAME
+            // 
+            this.TABLE_NAME.DataPropertyName = "TABLE_NAME";
+            this.TABLE_NAME.HeaderText = "TABLE_NAME";
+            this.TABLE_NAME.Name = "TABLE_NAME";
+            this.TABLE_NAME.Width = 150;
+            // 
+            // COLUMN_NAME
+            // 
+            this.COLUMN_NAME.DataPropertyName = "COLUMN_NAME";
+            this.COLUMN_NAME.HeaderText = "COLUMN_NAME";
+            this.COLUMN_NAME.Name = "COLUMN_NAME";
+            this.COLUMN_NAME.Width = 150;
+            // 
+            // COMMENTS
+            // 
+            this.COMMENTS.DataPropertyName = "COMMENTS";
+            this.COMMENTS.HeaderText = "COMMENTS";
+            this.COMMENTS.Name = "COMMENTS";
+            this.COMMENTS.Width = 400;
+            // 
+            // btn_CreateCode
+            // 
+            this.btn_CreateCode.Location = new System.Drawing.Point(691, 19);
+            this.btn_CreateCode.Name = "btn_CreateCode";
+            this.btn_CreateCode.Size = new System.Drawing.Size(75, 23);
+            this.btn_CreateCode.TabIndex = 0;
+            this.btn_CreateCode.Text = "生成SQL";
+            this.btn_CreateCode.UseVisualStyleBackColor = true;
+            this.btn_CreateCode.Click += new System.EventHandler(this.btn_CreateCode_Click);
+            // 
+            // cbb_Type
+            // 
+            this.cbb_Type.FormattingEnabled = true;
+            this.cbb_Type.Items.AddRange(new object[] {
+            "table",
+            "column"});
+            this.cbb_Type.Location = new System.Drawing.Point(585, 20);
+            this.cbb_Type.Name = "cbb_Type";
+            this.cbb_Type.Size = new System.Drawing.Size(100, 20);
+            this.cbb_Type.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 455);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_List)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv_List;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TABLE_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COLUMN_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COMMENTS;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_FilePath;
+        private System.Windows.Forms.Button btn_LoadData;
+        private System.Windows.Forms.Button btn_SelectFile;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox rtb_View;
+        private System.Windows.Forms.Button btn_CreateCode;
+        private System.Windows.Forms.ComboBox cbb_Type;
+    }
+}
+
---
+++ AutoCodeToComments/AutoCodeToComments/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoCodeToComments
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            if (OFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tb_FilePath.Text = OFD.FileName;
+            }
+        }
+
+        private void btn_LoadData_Click(object sender, EventArgs e)
+        {
+            string path = tb_FilePath.Text;
+            if (string.IsNullOrEmpty(path)) return;
+
+            dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("TABLE_NAME");
+            dt.Columns.Add("COLUMN_NAME");
+            dt.Columns.Add("COMMENTS");
+            int i = 0;
+            String line = "";
+            try
+            {
+
+                StreamReader sr = new StreamReader(path, Encoding.Default);
+
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Console.WriteLine(line.ToString());
+                    string[] arr = line.Replace("\"", "").Split(',');
+                    if (arr.Length == 1)
+                        arr = line.Split('\t');
+                    DataRow dr = dt.NewRow();
+                    dr["ID"] = arr[0];
+                    dr["TABLE_NAME"] = arr[1];
+                    dr["COLUMN_NAME"] = arr[2];
+                    dr["COMMENTS"] = arr[3];
+                    dt.Rows.Add(dr);
+                    i++;
+                }
+
+                dgv_List.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("索引行: " + i.ToString() + "\n行内容:" + line + "\n错误信息: " + ex.Message);
+                //throw;
+            }
+        }
+
+        private void btn_CreateCode_Click(object sender, EventArgs e)
+        {
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                string strCodes = "";
+
+                var dt1 = (DataTable)dgv_List.DataSource;
+                var dv = dt1.DefaultView;
+                dt = dv.ToTable();
+
+                if (cbb_Type.SelectedItem.ToString() == "table")
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (!string.IsNullOrEmpty(dr["COMMENTS"].ToString()))
+                            strCodes += string.Format("comment on table \"{0}\"  is '{1}';\n", dr["TABLE_NAME"], dr["COMMENTS"]);
+                    }
+                }
+                else
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (!string.IsNullOrEmpty(dr["COMMENTS"].ToString()))
+                            strCodes += string.Format("comment on column \"{0}\".\"{1}\"  is '{2}';\n", dr["TABLE_NAME"], dr["COLUMN_NAME"], dr["COMMENTS"]);
+                    }
+                }
+
+                rtb_View.Text = strCodes;
+                tabControl1.SelectTab(1);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbb_Type.SelectedIndex = 0;
+        }
+    }
+}
